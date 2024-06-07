4afb595e76582c6292291d62f005493401a6b5dd
AgusAhi <aluahi5617@ieselcaminas.org>
2024-06-07 17:44:59 +0200

    commit

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-dark-mode');
+    const body = document.body;
+    const modeText = document.getElementById('modeText');
 
-// Obtener el botón y el elemento body
-const toggleButton = document.getElementById('toggle-dark-mode');
-const body = document.body;
-
-// Añadir un evento de clic al botón
-toggleButton.addEventListener('click', () => {
-    // Alternar la clase 'dark-mode' en el body
-    body.classList.toggle('dark-mode');
-
-    // Cambiar el texto del botón dependiendo del modo
-    if (body.classList.contains('dark-mode')) {
-        toggleButton.textContent = 'Modo Claro';
-    } else {
-        toggleButton.textContent = 'Modo Oscuro';
-    }
+    toggleButton.addEventListener('click', () => {
+        if (body.classList.contains('dark-mode')) {
+            body.classList.remove('dark-mode');
+            body.classList.add('light-mode');
+            toggleButton.querySelector('img').src = 'images/blanco.png';
+            toggleButton.querySelector('img').alt = 'Modo Oscuro';
+            modeText.textContent = 'Modo Oscuro';
+        } else {
+            body.classList.remove('light-mode');
+            body.classList.add('dark-mode');
+            toggleButton.querySelector('img').src = 'images/negro.png';
+            toggleButton.querySelector('img').alt = 'Modo Claro';
+            modeText.textContent = 'Modo Claro';
+        }
+    });
 });
